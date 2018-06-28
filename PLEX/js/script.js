36eaaa63c8fa43fc6b5ad9a0c36eeaf079897e4e
lexandor <plex-team@mail.ru>
2018-06-28 22:55:46 +0400

    ReFresh Green

--- PLEX/js/script.js
+++ PLEX/js/script.js
@@ -1,8 +1,33 @@
 
-window.onload = function () {
+// window.onload = function () {
 
-    var div = document.getElementById('vidOff');
-    div.onclick = function () {
-        div.style.display = 'none';
-    };
-};
+//     var div = document.getElementById('vidOff');
+//     div.onclick = function () {
+//         div.style.display = 'none';
+//     };
+// };
+
+// $(window).on('load', function(event) {
+	
+// 	var wrap = $('vidOff');
+// 	wrap.on('click', function(event) {
+// 		event.preventDefault();
+// 		console.log('1');
+// 		wrap.style.display = 'none';
+// 	});
+// });
+
+$(document).ready(function() {
+	
+	var navBtn = $('.menu_item_btn');
+	var menu = $('.menu_item');
+
+	navBtn.on('click', function(event) {
+		event.preventDefault();
+		console.log('click');
+
+		menu.toggleClass('menu_active');
+
+	});
+	
+});
